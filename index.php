fe8c238c8cba83c343c6eec066a3f6a886e87234
hanifyudhaa <hanifyudhaa@gmail.com>
2020-04-22 15:23:58 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur perspiciatis, quam natus doloremque repellendus soluta fugiat esse asperiores incidunt fugit quo, libero consectetur voluptatibus, cupiditate dolorem fuga recusandae tempore odio.</p>
 </body>
 </html>
